47d35094e443b21fdd10914ffdb5acf5ff809850
Karnica2013 <karnicasrir1@educbe.ca>
2025-02-22 19:48:35 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let rn =Math.floor(Math.random()*10)
+function checkAnswer(){
+   let userNumber = document.getElementById("userNumber").value
+   if(userNumber>10 || userNumber<0){
+    alert("Number out of range!!!")
+    return
+   }
+   if(userNumber==rn){
+    document.getElementById("p2").innerHTML="  <p>Congratulations! You've Won!! </p><button onclick='resetGame()'>Play Again</button>"
+   }
+   else if(userNumber<rn){
+     document.getElementById("p2").innerHTML="  <p>Higher! Try Again</p>"
+   }
+   else{
+    document.getElementById("p2").innerHTML="  <p>Lower! Try Again</p>"
+   }
+}
+function resetGame(){
+    window.location="index.html"
+}
